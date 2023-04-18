705bbc0f5447576807c344495263883d4848d468
grahamjantz <grahammrjantz@gmail.com>
2023-04-18 13:18:03 -0600

    further testing

--- numbs.py
+++ numbs.py
@@ -21,7 +21,7 @@ else:
         total_books_list.append(tmp_list)
 
 
-#89, 187, 78, 65, 62, 150, 31, 249, 250, 28
+# 89 187 78 65 62 150 31 249 250 28
 
 for item in total_books_list:
     book = []
@@ -34,9 +34,18 @@ print("Thank you, calculating how long your reading cycle will be!")
 
 master_list = []
 complete = False
-index = 0
+# index = 0 + 1000000000
+# day = 1 + 1000000000
+index = 0 
 day = 1
 key = []
+
+# 1500000 [83, 73, 60, 60, 34, 150, 30, 24, 250, 12]
+# 1500000 [83, 73, 60, 60, 34, 150, 30, 24, 250, 12]
+
+# numbs.py ran on my phone for this many cycles with no match. tested running program starting at 100,000 less than this value and it produced the same temp_list value so it works to add to the index and day values to 'skip' ahead in the program running to get closer to the answer see down VVVV
+# 896600000 [27, 76, 62, 10, 20, 50, 20, 53, 250, 16]
+# 896600000 [27, 76, 62, 10, 20, 50, 20, 53, 250, 16]
 
 for i in total_books_list:
     key.append(1)
@@ -65,8 +74,16 @@ while complete == False:
         complete = True
         print('Cycle will complete on day:', str(day))
     
-    # if day == 9000000:
-    #     master_list = []
+    if day == 2000000000:
+        complete = True
+        print('Cycled through 2,000,000,000 days. Did not find end')
+    elif day == 3000000000:
+        complete = True
+        print('Cycled through 3,000,000,000 days. Did not find end')
+    elif day == 4000000000:
+        complete = True
+        print('Cycled through 4,000,000,000 days. Did not find end')
+
         
     
 
