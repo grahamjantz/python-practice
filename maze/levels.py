7b55bafcc0b5cede15889b233437b13829f53bd7
grahamjantz <grahammrjantz@gmail.com>
2023-05-17 17:05:54 -0600

    added 4 levels to maze

---
+++ maze/levels.py
@@ -0,0 +1,114 @@
+global_char_x_pos = 1
+global_char_y_pos = 0
+
+PATH = '░'
+
+back = '█'
+wall = '#'
+char = 'X'
+endd = 'O'
+
+ver1 = '║'
+ver2 = '╣'
+ver3 = '╠'
+
+hor1 = '═'
+hor2 = '╦'
+hor3 = '╩'
+
+cor1 = '╔'
+cor2 = '╗'
+cor3 = '╝'
+cor4 = '╚'
+
+cros = '╬'
+
+level_one = [
+    [cor1, hor1, hor1, hor1, hor1, hor1, cor2],
+    [ver1, PATH, PATH, PATH, PATH, PATH, ver1],
+    [ver3, hor1, hor1, hor1, hor1, PATH, ver1],
+    [ver1, PATH, PATH, PATH, PATH, PATH, ver1],
+    [ver1, PATH, hor1, hor1, hor1, hor1, ver2],
+    [ver1, PATH, PATH, PATH, PATH, PATH, ver1],
+    [cor4, hor1, hor1, hor1, hor1, endd, cor3]
+]
+
+level_two = [
+    [cor1, hor1, hor1, hor1, hor1, hor1, hor1, hor2, hor1, hor2, hor1, cor2],
+    [ver1, PATH, PATH, PATH, PATH, PATH, PATH, ver1, PATH, ver1, PATH, ver1],
+    [ver3, hor1, hor1, cor2, PATH, ver1, PATH, ver1, PATH, ver1, PATH, ver1],
+    [ver1, PATH, PATH, ver1, PATH, cor4, hor1, cor3, PATH, ver1, PATH, ver1],
+    [ver3, hor1, PATH, ver1, PATH, PATH, PATH, PATH, PATH, PATH, PATH, ver1],
+    [ver1, PATH, PATH, cor4, hor1, PATH, ver1, PATH, ver1, PATH, hor1, ver2],
+    [ver1, PATH, PATH, PATH, PATH, PATH, ver1, PATH, ver1, PATH, PATH, ver1],
+    [cor4, hor1, hor1, hor1, hor1, hor1, hor3, hor1, hor3, hor1, endd, cor3],
+]
+
+level_three = [
+    [cor1, hor1, hor1, hor1, hor1, hor2, hor1, hor2, hor1, hor1, hor1, hor1, hor1, hor1, cor2, ],
+    [ver1, PATH, PATH, PATH, PATH, ver1, PATH, ver1, PATH, PATH, PATH, PATH, PATH, PATH, ver1, ],
+    [ver3, hor1, hor1, hor1, PATH, ver1, PATH, cor4, hor1, PATH, PATH, cor1, hor1, PATH, ver1, ],
+    [ver1, PATH, PATH, PATH, PATH, ver1, PATH, PATH, PATH, PATH, cor1, cor3, PATH, PATH, ver1, ],
+    [ver3, hor1, hor1, hor1, PATH, ver1, PATH, ver1, PATH, hor1, cros, hor1, PATH, hor1, ver2, ],
+    [ver1, PATH, PATH, PATH, PATH, ver1, PATH, ver1, PATH, PATH, ver1, PATH, PATH, PATH, ver1, ],
+    [ver1, PATH, hor1, hor1, hor1, cor3, PATH, cor4, hor2, hor1, hor3, hor1, hor1, PATH, ver1, ],
+    [ver1, PATH, PATH, PATH, PATH, PATH, PATH, PATH, ver1, PATH, PATH, PATH, PATH, PATH, ver1, ],
+    [ver3, hor1, hor1, hor1, hor1, hor1, hor1, hor1, cor3, PATH, hor1, hor1, hor1, hor1, ver2, ],
+    [ver1, PATH, PATH, PATH, PATH, PATH, PATH, PATH, PATH, PATH, PATH, PATH, PATH, PATH, ver1, ],
+    [ver1, PATH, hor1, hor1, hor1, hor1, hor2, hor1, hor1, hor1, hor1, hor1, hor1, PATH, ver1, ],
+    [ver1, PATH, PATH, PATH, PATH, PATH, ver1, PATH, PATH, PATH, PATH, PATH, PATH, PATH, ver1, ],
+    [ver3, hor1, hor1, PATH, hor1, hor1, cros, hor1, hor1, PATH, cor1, hor1, hor1, hor1, ver2, ],
+    [ver1, PATH, PATH, PATH, PATH, PATH, ver1, PATH, PATH, PATH, ver1, PATH, PATH, PATH, ver1, ],
+    [ver1, PATH, cor1, hor1, hor1, hor1, cor3, PATH, cor1, hor1, cor3, PATH, ver1, PATH, ver1, ],
+    [ver1, PATH, ver1, PATH, PATH, PATH, PATH, PATH, ver1, PATH, PATH, PATH, ver1, PATH, ver1, ],
+    [ver1, PATH, ver1, PATH, hor1, hor1, hor1, hor1, cor3, PATH, ver1, PATH, ver1, PATH, ver1, ],
+    [ver1, PATH, ver1, PATH, PATH, PATH, PATH, PATH, PATH, PATH, ver1, PATH, ver1, PATH, ver1, ],
+    [cor4, hor1, hor3, hor1, hor1, hor1, hor1, hor1, hor1, hor1, hor3, hor1, hor3, endd, cor3, ],
+]
+level_four = [
+    [cor1, hor1, hor2, hor1, hor1, hor1, hor1, hor1, hor1, hor1, hor1, hor1, hor1, hor1, hor1, hor1, hor1, hor1, hor1, hor2, hor1, hor1, hor1, hor1, hor2, hor1, hor1, hor1, hor1, hor1, cor2, ],
+    [ver1, PATH, ver1, PATH, PATH, PATH, PATH, PATH, PATH, PATH, PATH, PATH, PATH, PATH, PATH, PATH, PATH, PATH, PATH, ver1, PATH, PATH, PATH, PATH, ver1, PATH, PATH, PATH, PATH, PATH, ver1, ],
+    [ver1, PATH, ver1, PATH, ver1, PATH, cor1, hor1, hor1, hor1, hor2, hor1, hor1, hor1, hor1, hor1, PATH, hor1, hor1, cor3, PATH, cor1, hor1, PATH, ver1, PATH, cor1, hor1, cor2, PATH, ver1, ],
+    [ver1, PATH, ver1, PATH, ver1, PATH, ver1, PATH, PATH, PATH, ver1, PATH, PATH, PATH, PATH, PATH, PATH, PATH, PATH, PATH, PATH, ver1, PATH, PATH, ver1, PATH, ver1, PATH, ver1, PATH, ver1, ],
+    [ver1, PATH, ver1, PATH, ver1, PATH, ver1, PATH, ver1, PATH, PATH, PATH, cor1, hor1, PATH, cor1, hor1, hor1, hor1, hor1, hor1, cor3, PATH, cor1, cor3, PATH, ver1, PATH, ver1, PATH, ver1, ],
+    [ver1, PATH, ver1, PATH, ver1, PATH, PATH, PATH, ver3, hor1, hor1, hor1, ver2, PATH, PATH, ver1, PATH, PATH, PATH, PATH, PATH, PATH, PATH, ver1, PATH, PATH, ver1, PATH, ver1, PATH, ver1, ],
+    [ver1, PATH, ver1, PATH, ver1, PATH, hor1, hor1, ver2, PATH, PATH, PATH, ver1, PATH, cor1, hor3, hor1, hor1, hor1, hor1, cor2, PATH, PATH, ver1, PATH, cor1, cor3, PATH, ver1, PATH, ver1, ],
+    [ver1, PATH, PATH, PATH, ver1, PATH, PATH, PATH, cor4, hor1, hor1, PATH, ver1, PATH, ver1, PATH, PATH, PATH, PATH, PATH, ver1, PATH, hor1, cor3, PATH, ver1, PATH, PATH, ver1, PATH, ver1, ],
+    [ver1, PATH, cor1, hor1, hor3, hor1, cor2, PATH, PATH, PATH, PATH, PATH, ver1, PATH, ver1, PATH, cor1, hor1, cor2, PATH, PATH, PATH, PATH, PATH, PATH, ver1, PATH, cor1, cor3, PATH, ver1, ],
+    [ver1, PATH, ver1, PATH, PATH, PATH, cor4, hor1, hor2, hor1, hor2, hor1, cor3, PATH, cor4, PATH, ver1, PATH, ver1, PATH, cor1, hor1, hor1, hor1, hor1, cor3, PATH, PATH, PATH, PATH, ver1, ],
+    [ver1, PATH, ver1, PATH, ver1, PATH, PATH, PATH, ver1, PATH, ver1, PATH, PATH, PATH, PATH, PATH, ver1, PATH, cor4, hor1, ver2, PATH, PATH, PATH, PATH, PATH, PATH, PATH, cor1, hor1, ver2, ],
+    [ver1, PATH, PATH, PATH, ver1, PATH, cor1, PATH, ver1, PATH, ver1, PATH, cor1, hor1, hor1, hor1, cor3, PATH, PATH, PATH, ver1, PATH, ver1, PATH, ver1, PATH, PATH, hor1, cor3, PATH, ver1, ],
+    [ver3, hor1, hor1, hor1, ver2, PATH, ver1, PATH, PATH, PATH, ver1, PATH, ver1, PATH, PATH, PATH, PATH, PATH, ver1, PATH, cor4, hor1, hor3, hor1, ver2, PATH, PATH, PATH, PATH, PATH, ver1, ],
+    [ver1, PATH, PATH, PATH, cor4, hor1, cros, hor1, hor1, PATH, ver1, PATH, ver1, PATH, cor1, hor1, hor1, hor1, cor3, PATH, PATH, PATH, PATH, PATH, ver1, PATH, ver1, PATH, ver1, PATH, ver1, ],
+    [ver1, PATH, ver1, PATH, PATH, PATH, ver1, PATH, PATH, PATH, ver1, PATH, cor4, hor1, cor3, PATH, PATH, PATH, PATH, PATH, cor1, hor1, hor1, PATH, ver3, hor1, hor3, hor1, ver2, PATH, ver1, ],
+    [ver1, PATH, cor4, hor1, cor2, PATH, PATH, PATH, ver1, PATH, ver1, PATH, PATH, PATH, PATH, PATH, cor1, hor1, hor1, PATH, ver1, PATH, PATH, PATH, ver1, PATH, PATH, PATH, ver1, PATH, ver1, ],
+    [ver1, PATH, PATH, PATH, cor4, hor1, hor2, hor1, cor3, PATH, cor4, hor1, hor1, cor2, PATH, cor1, cor3, PATH, PATH, PATH, cor4, hor1, hor1, hor1, hor3, hor1, hor1, PATH, ver1, PATH, ver1, ],
+    [ver1, PATH, PATH, PATH, PATH, PATH, ver1, PATH, PATH, PATH, PATH, PATH, PATH, ver1, PATH, ver1, PATH, PATH, ver1, PATH, PATH, PATH, PATH, PATH, PATH, PATH, PATH, PATH, ver1, PATH, ver1, ],
+    [cor4, hor1, hor1, hor1, hor1, hor1, hor3, hor1, hor1, hor1, hor1, hor1, hor1, hor3, hor1, hor3, hor1, hor1, hor3, hor1, hor1, hor1, hor1, hor1, hor1, hor1, hor1, hor1, hor3, endd, cor3, ],
+]
+
+
+levels = [level_one, level_two, level_three, level_four] 
+
+
+
+def print_maze(maze):
+    maze[global_char_y_pos][global_char_x_pos] = char
+    for row in maze:
+        print('\t\t', end='')
+        for block in row:
+            print(block, end='')
+        print('')
+    print('\n')
+
+
+def print_all_levels(levs):
+    count = 1
+    for level in levs:
+        print('Level ', count, ':')
+        print_maze(level)
+        count += 1
+
+# print_all_levels(levels)
+
+# print_maze(level_four)
--- maze/maze.py
+++ maze/maze.py
@@ -1,3 +1,5 @@
+from levels import levels
+
 print("Welcome to the Maze!\n")
 print('Your goal is to get your character: X to the exit: O without touching any of the walls: #\n')
 print('Good luck!\n')
@@ -6,56 +8,12 @@ PATH = '░'
 wall = '#'
 char = 'X'
 end = 'O'
-vert = '║'
-hor = '═'
-cor1 = '╔'
-cor2 = '╗'
-cor3 = '╝'
-cor4 = '╚'
-# PATH = 'v'
+
 current_level = 1
 global_play_game = True
 global_char_x_pos = 1
 global_char_y_pos = 0
 
-level_one = [
-    [cor1, hor, hor, hor, hor, hor, cor2],
-    [vert, PATH, PATH, PATH, PATH, PATH, vert],
-    ['╠', hor, hor, hor, hor, PATH, vert],
-    [vert, PATH, PATH, PATH, PATH, PATH, vert],
-    [vert, PATH, hor, hor, hor, hor, '╣'],
-    [vert, PATH, PATH, PATH, PATH, PATH, vert],
-    [cor4, hor, hor, hor, hor, end, cor3]
-]
-
-level_two = [
-    [cor1, hor, hor, hor, hor, hor, hor, hor, hor, hor, hor, cor2],
-    [vert, PATH, wall, wall, wall, wall, wall, wall, wall, wall, wall, vert],
-    [vert, PATH, PATH, PATH, PATH, PATH, PATH, wall, wall, PATH, wall, vert],
-    ['╠', hor, hor, cor2, PATH, wall, PATH, wall, wall, PATH, wall, vert],
-    [vert, PATH, PATH, vert, PATH, wall, wall, wall, wall, PATH, PATH, vert],
-    ['╠', hor, PATH, vert, PATH, PATH, PATH, PATH, PATH, PATH, PATH, vert],
-    [vert, PATH, PATH, cor4, hor, PATH, vert, PATH, vert, PATH, hor, '╣'],
-    [vert, PATH, PATH, PATH, PATH, PATH, vert, PATH, vert, PATH, PATH, vert],
-    [cor4, hor, hor, hor, hor, hor, '╩', hor, '╩', hor, end, cor3],
-]
-
-level_three = [
-    [PATH, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall],
-    [PATH, PATH, wall, wall, wall, wall, wall, PATH, PATH, PATH, wall, PATH, PATH, PATH, wall],
-    [wall, PATH, wall, PATH, PATH, PATH, wall, PATH, wall, PATH, PATH, PATH, wall, PATH, wall],
-    [wall, PATH, PATH, PATH, wall, wall, wall, PATH, wall, wall, wall, wall, wall, PATH, wall],
-    [wall, PATH, wall, wall, PATH, PATH, wall, PATH, wall, PATH, PATH, PATH, PATH, PATH, wall],
-    [wall, PATH, wall, PATH, wall, PATH, wall, PATH, wall, PATH, wall, wall, wall, wall, wall],
-    [wall, PATH, wall, PATH, wall, PATH, wall, PATH, wall, PATH, PATH, PATH, PATH, PATH, wall],
-    [wall, PATH, PATH, PATH, wall, PATH, wall, PATH, wall, wall, wall, wall, wall, PATH, wall],
-    [wall, PATH, wall, PATH, PATH, PATH, wall, PATH, wall, PATH, PATH, PATH, PATH, PATH, wall],
-    [wall, PATH, PATH, PATH, PATH, PATH, PATH, PATH, wall, wall, wall, wall, wall, PATH, wall],
-    [wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, PATH, end],
-]
-
-
-levels = [level_two, level_three] 
 
 def print_maze(maze):
     maze[global_char_y_pos][global_char_x_pos] = char
@@ -72,11 +30,11 @@ def hit_wall():
     print('Lose! You hit a wall!')
     
 def win():
-    print('//==========-----WIN-----==========//')
+    print('\n\t//═══════════════WIN═══════════════//\n')
 
 def move_up(maze, char_x_pos, char_y_pos):
     if char_y_pos >= 0:
-        if maze[char_y_pos - 1][char_x_pos] == 'O':
+        if maze[char_y_pos - 1][char_x_pos] == end:
             win()
             return 2
         elif maze[char_y_pos - 1][char_x_pos] != PATH:
@@ -89,7 +47,7 @@ def move_up(maze, char_x_pos, char_y_pos):
     
 def move_down(maze, char_x_pos, char_y_pos):
     if char_y_pos < len(maze) - 1:
-        if maze[char_y_pos + 1][char_x_pos] == 'O':
+        if maze[char_y_pos + 1][char_x_pos] == end:
             win()
             return 2
         elif maze[char_y_pos + 1][char_x_pos] != PATH:
@@ -102,7 +60,7 @@ def move_down(maze, char_x_pos, char_y_pos):
 
 def move_left(maze, char_x_pos, char_y_pos):
     if char_x_pos > -1:
-        if maze[char_y_pos][char_x_pos -1] == 'O':
+        if maze[char_y_pos][char_x_pos -1] == end:
             win()
             return 2
         elif maze[char_y_pos][char_x_pos - 1] != PATH:
@@ -115,7 +73,7 @@ def move_left(maze, char_x_pos, char_y_pos):
 
 def move_right(maze, char_x_pos, char_y_pos):
     if char_x_pos < len(maze[0]):
-        if maze[char_y_pos][char_x_pos + 1] == 'O':
+        if maze[char_y_pos][char_x_pos + 1] == end:
             win()
             return 2
         elif maze[char_y_pos][char_x_pos + 1] != PATH:
@@ -130,17 +88,17 @@ def move_right(maze, char_x_pos, char_y_pos):
 
 
 while global_play_game == True:
-    print('\n\tCurrent Level: Level ', current_level, '\n')
+    print('\n\t\tCurrent Level: Level ', current_level, '\n')
     play_level = levels[current_level - 1]
     print_maze(play_level)
-    move = input('\n\t    Move: W,A,S,D: \n\t    ')
+    move = input('\n\t\t    Move: W,A,S,D: \n\t\t    ')
 
     if move == 'W' or move == 'w':
         res = move_up(play_level, global_char_x_pos, global_char_y_pos)
         if res == False:
             global_play_game = False
         elif res == 2:
-            global_char_x_pos = 0
+            global_char_x_pos = 1
             global_char_y_pos = 0
             current_level += 1
         else:
@@ -151,7 +109,7 @@ while global_play_game == True:
         if res == False:
             global_play_game = False
         elif res == 2:
-            global_char_x_pos = 0
+            global_char_x_pos = 1
             global_char_y_pos = 0
             current_level += 1
         else:
@@ -165,7 +123,7 @@ while global_play_game == True:
             global_char_y_pos += 1
             play_level[global_char_y_pos - 1][global_char_x_pos] = PATH
         elif res == 2:
-            global_char_x_pos = 0
+            global_char_x_pos = 1
             global_char_y_pos = 0
             current_level += 1
     elif move == 'D' or move == 'd':
@@ -173,7 +131,7 @@ while global_play_game == True:
         if res == False:
             global_play_game = False
         elif res == 2:
-            global_char_x_pos = 0
+            global_char_x_pos = 1
             global_char_y_pos = 0
             current_level += 1
         else:
@@ -181,3 +139,6 @@ while global_play_game == True:
             play_level[global_char_y_pos][global_char_x_pos - 1] = PATH
     else:
         print('Invalid move! W,A,S,D to move!')
+    if current_level > len(levels):
+        print('\n\t\tThank you for playing! \n\tYou have beaten all the levels so far!\n')
+        global_play_game = False
